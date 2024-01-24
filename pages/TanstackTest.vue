750e48a08c019a90d54ecfa7ab9d69895092af58
Ivan Nelyubov <ivan.nelyubov@glovoapp.com>
2024-01-24 10:47:19 +0100

    TanstackTest

---
+++ pages/TanstackTest.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useQuery } from '@tanstack/vue-query'
+
+const fetcher = async () =>
+    await fetch('https://jsonplaceholder.typicode.com/posts').then((response) =>
+        response.json(),
+    )
+
+const { data, suspense } = useQuery({ queryKey: ['test'], queryFn: fetcher })
+
+await suspense()
+</script>
+<template>
+  <div>{{ data }}</div>
+</template>
